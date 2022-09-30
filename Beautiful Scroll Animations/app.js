2b09dade82f08c0bdc4fbff6aafce326d2b9b826
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2022-09-30 19:03:16 +0530

    Added CSS Scroll Animation

---
+++ Beautiful Scroll Animations/app.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
